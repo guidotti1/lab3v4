926b1ff61b212039eba918111df224c21e3a4449
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-11-07 12:35:36 -0500

    Update and rename PhoneEntry.cpp to artEntry.cpp

---
+++ artEntry.cpp
@@ -0,0 +1,26 @@
+
+#include "artEntry.h"
+
+artEntry::artEntry() {
+}
+
+artEntry::artEntryartEntry(string name, string bornDiedInit, string titleInit, string dateInit, string techniqueInit, string locationInit, string urlInit, string formInit, string typeInit, string schoolInit, string timeframeInit) 
+{
+    stringstream nameStream(name);
+    getline(nameStream, last, ',');
+    getline(nameStream, first);
+    bornDied = bornDiedInit;
+    title = titleInit;
+    date = dateInit;
+    technique = techniqueInit;
+    location = locationInit;
+    url = urlInit;
+    form = formInit;
+    type = typeInit;
+    school = schoolInit;
+    timeframe = timeframeInit;
+}
+
+
+#endif /* PHONEENTRY_H */
+
