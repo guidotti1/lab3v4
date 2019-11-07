af44c8f2ade060846b1950e273d977955e06036a
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-11-07 12:27:57 -0500

    Update and rename PhoneEntry.h to artEntry.h

---
+++ artEntry.h
@@ -0,0 +1,29 @@
+#include <string>
+using namespace std;
+
+#ifndef ARTENTRY_H
+#define ARTENTRY_H
+
+class artEntry {
+public:
+     artEntry();
+     artEntry(string name, string bornDiedInit, string titleInit, string dateInit, string techniqueInit, string locationInit, string urlInit, string formInit, string typeInit, string schoolInit, string timeframeInit);
+     string last;
+     string first;
+     string bornDied;
+     string title;
+     string date;
+     string technqiue;
+     string location;
+     string url;
+     string form;
+     string type;
+     string school;
+     string timeframe;
+
+private:
+
+};
+
+#endif /* PHONEENTRY_H */
+
