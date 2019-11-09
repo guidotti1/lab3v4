caabf1282491eecec459688c0bc260d75a445d08
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-11-09 17:01:35 -0500

    Add files via upload

---
+++ userComment.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <stdlib.h>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+// Stuff for AJAX
+#include "cgicc/Cgicc.h"
+#include "cgicc/HTTPHTMLHeader.h"
+#include "cgicc/HTMLClasses.h"
+#include "mysql_connection.h"
+#include "mysql_driver.h"
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
+
+
+#define XML_USE_STL
+
+#define HOST "localhost"
+#define USER "guidotti1"
+#define DB "guidotti1"
+#define PASS "S217026"
+
+using namespace std;
+using namespace cgicc; // Needed for AJAX functions.
+
+int main()
+{
+    const string url=HOST;
+    const string user=USER;
+    const string database=DB;
+    const string pass=PASS;
+
+    Cgicc cgi;
+    form_iterator comment = cgi.getElement("comment");
+
+    cout << "Content-Type: text/plain\n\n";
+    sql::Driver* driver = sql::mysql::get_driver_instance();
+    std::auto_ptr<sql::Connection> con(driver->connect(url, user, pass));
+    con->setSchema(database);
+    std::auto_ptr<sql::Statement> stmt(con->createStatement());
+    vector<userInfo> list;
+    std::auto_ptr< sql::ResultSet > res;
+
+    return 0;
+}
