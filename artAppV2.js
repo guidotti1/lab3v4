345bd70657a58e0392c93d930c9a8ac4deedc613
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-11-10 11:59:13 -0500

    Update artAppV2.js

--- artAppV2.js
+++ artAppV2.js
@@ -43,7 +43,7 @@ $(document).ready(function () {
 		error: function(){alert("Error: Something went wrong");}
     		});
 	   });
-    $("#ReturnHomeBtn").click()
+    $("#ReturnHomeBtn").click(function()
 	{
 		$("#ViewArtPage").hide();
 		$("Home").show();
