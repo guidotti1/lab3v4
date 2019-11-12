7ca7934869e35ea8c817265e7a3693bf2cb7d102
levar1 <38579890+erlevar@users.noreply.github.com>
2019-11-12 15:12:48 -0500

    Update artEntry.cpp

--- artEntry.cpp
+++ artEntry.cpp
@@ -2,6 +2,20 @@
 #include "artEntry.h"
 
 artEntry::artEntry() {
+    //empty constructor to avoid initialization errors
+	last="";
+	first="";
+	bornDied="";
+	title="";
+	date="";
+	technique="";
+	location="";
+	url="";
+	form="";
+	type="";
+	school="";
+	timeframe="";
+	ARTID="";
 }
 
 artEntry::artEntry(string name, string bornDiedInit, string titleInit, string dateInit, string techniqueInit, string locationInit, string urlInit, string formInit, string typeInit, string schoolInit, string timeframeInit, string ARTIDInit) 
