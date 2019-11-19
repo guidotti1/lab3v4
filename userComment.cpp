59add187171c2fbb4b5a189a5182ad8fccf2ddf2
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-11-19 17:04:22 -0500

    Update userComment.cpp

--- userComment.cpp
+++ userComment.cpp
@@ -71,7 +71,7 @@ int main()
     {
         stmt->execute("INSERT INTO comments(Email, ARTID, Comment) VALUES('"+emailString+"', '"+artIDString+"', '"+commentString+"')");
         //USED TO GET THE MOST RECENT COMMENTID
-        stmt->execute("SELECT * FROM comments WHERE ARTID = '"+artIDString+"' and Comment = '"commentString+"' and Email = '"emailString"'");
+        stmt->execute("SELECT * FROM comments WHERE ARTID = '"+artIDString+"' and Comment = '"+commentString+"' and Email = '"+emailString+"'");
         do {
             res.reset(stmt->getResultSet());
             while (res->next()) {
