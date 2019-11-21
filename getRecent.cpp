47be019b303f64d3f5982f7a0e752cecb88cd55a
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-11-21 16:33:58 -0500

    Update getRecent.cpp

--- getRecent.cpp
+++ getRecent.cpp
@@ -146,7 +146,7 @@ int main()
             do {
                      res.reset(stmt->getResultSet());
                      while (res->next()) {
-                       upvotedArt.push_back(res->getString("ARTID"));
+                       upvotedArt.push_back(res->getString("voteID"));
                      }
                 }while (stmt ->getMoreResults());
 
@@ -176,7 +176,7 @@ int main()
             do {
                      res.reset(stmt->getResultSet());
                      while (res->next()) {
-                       downvotedArt.push_back(res->getString("ARTID"));
+                       downvotedArt.push_back(res->getString("voteID"));
                      }
                 }while (stmt ->getMoreResults());
 
