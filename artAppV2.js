79471c1232d20224fad83a523457050d111d4c23
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-11-21 16:43:23 -0500

    Update artAppV2.js

--- artAppV2.js
+++ artAppV2.js
@@ -19,12 +19,16 @@ $(document).ready(function () {
     $("#LogOutRecentPage").hide();
     $("#AddFriendBtn").hide();
 	
-    $.ajax({
+     if (typeof initLastCommentId === "undefined")
+	{
+		    $.ajax({
 	url: '/cgi-bin/guidotti1_getLastCommentID.cgi?lastcomment='+0,
 	dataType: 'text',
 	success: initLastCommentId,
 	error: function(){alert("Error: Something went wrong");}
      });
+	 }
+
 	
     $("#LogOutBtnView").click(function()
 	{
